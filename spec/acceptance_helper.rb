3842333837a50239d475550c21365873ef732dd2
Matis Masters <matis@craftedcode.com>
2016-07-04 03:23:34 -0300

    Remove Host and Cookies header from curl examples on docs

--- spec/acceptance_helper.rb
+++ spec/acceptance_helper.rb
@@ -4,7 +4,11 @@ require 'rspec_api_documentation/dsl'
 
 RspecApiDocumentation.configure do |config|
   config.format = :json
-  config.curl_host = ENV['API_URL'] || 'http://localhost:3000'
+
+  protocol = ENV['RAILS_ENV'] == 'production' ? 'https' : 'http'
+  config.curl_host = "#{protocol}://#{ENV['HOST_URL'] || 'localhost:3000'}"
+
+  config.curl_headers_to_filter = %w(Cookie Host)
   config.api_name = 'Mr.Wattson API'
   config.request_body_formatter = proc do |params|
     params.empty? ? nil : params.to_json
