ede3d025f6c66fef4143bb04c5031a4dec5f0d54
Matis Masters <matis@craftedcode.com>
2016-07-04 19:39:52 -0300

    New docs curl url

--- spec/acceptance_helper.rb
+++ spec/acceptance_helper.rb
@@ -4,10 +4,7 @@ require 'rspec_api_documentation/dsl'
 
 RspecApiDocumentation.configure do |config|
   config.format = :json
-
-  protocol = ENV['RAILS_ENV'] == 'production' ? 'https' : 'http'
-  config.curl_host = "#{protocol}://#{ENV['HOST_URL'] || 'localhost:3000'}"
-
+  config.curl_host = 'https://mrwattson-api.herokuapp.com'
   config.curl_headers_to_filter = %w(Cookie Host)
   config.api_name = 'Mr.Wattson API'
   config.request_body_formatter = proc do |params|
