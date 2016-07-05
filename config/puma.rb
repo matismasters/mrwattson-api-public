f1e6afdbabebd535e69e2cf4def2ce7a2f20c0c0
Matis Masters <matis@craftedcode.com>
2016-07-05 03:37:08 -0300

    PUMA configuration for extra threading"

---
+++ config/puma.rb
@@ -0,0 +1,13 @@
+workers Integer(ENV['WEB_CONCURRENCY'] || 10)
+threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 10)
+threads threads_count, threads_count
+
+preload_app!
+
+rackup      DefaultRackup
+port        ENV['PORT']     || 3000
+environment ENV['RACK_ENV'] || 'development'
+
+on_worker_boot do
+  ActiveRecord::Base.establish_connection
+end
