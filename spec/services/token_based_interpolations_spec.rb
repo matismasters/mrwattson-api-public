0fb6933386b3ae0ab13808716f0110477469dedb
Matis Masters <matis@craftedcode.com>
2016-08-13 15:39:08 -0300

    Add specs for text interpolation receiving empty value

--- spec/services/token_based_interpolations_spec.rb
+++ spec/services/token_based_interpolations_spec.rb
@@ -1,9 +1,17 @@
 require 'spec_helper'
 
 describe 'TokenBasedInterpolations', type: :unit do
-  describe 'With several tokens to replace in text' do
-    let(:tokens) { { 'asdf' => '123', 'fdsa' => '321' } }
+  let(:tokens) { { 'asdf' => '123', 'fdsa' => '321' } }
 
+  describe 'With empty text' do
+    it 'should return an empty String' do
+      result_text = TokenBasedInterpolations.interpolate(tokens, nil)
+
+      expect(result_text).to eq ''
+    end
+  end
+
+  describe 'With several tokens to replace in text' do
     it 'should replace all tokens for the corresponding value' do
       text = 'first instance {{asdf}}, second {{asdf}}, last {{fdsa}}'
 
