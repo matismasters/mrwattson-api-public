eea30ee8f820ea93fce0244fbb952bd3e2d9efb9
Matis Masters <matis@craftedcode.com>
2016-08-30 00:16:59 -0300

    Verify authenticity token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,9 @@
 class ApplicationController < ActionController::Base
-  include DeviseTokenAuth::Concerns::SetUserByToken
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :null_session
+  skip_before_action :verify_authenticity_token
+  include DeviseTokenAuth::Concerns::SetUserByToken
+
   before_action :set_json_format
   rescue_from ActionController::ParameterMissing,
     with: :parameter_missing_response
