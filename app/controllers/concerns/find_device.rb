a5ff3bac09e60c02f2157bc4652388fc21d1b3c2
Matis Masters <matis@craftedcode.com>
2016-10-24 22:16:39 -0300

    Add params read to 404

--- app/controllers/concerns/find_device.rb
+++ app/controllers/concerns/find_device.rb
@@ -4,12 +4,14 @@ module FindDevice
   def find_device
     @device = Device.where(id: params.require(:device_id)).first
 
+    puts params
     render json: { device_id: 'not found' }, status: 404 unless @device.present?
   end
 
   def find_device_by_particle_id
     @device = Device.find_by_particle_id(params.require(:device_id))
 
+    puts params
     render json: { device_id: 'not found' }, status: 404 unless @device.present?
   end
 end
