91940dd485c2dd1d56cc793e7f89635ffcef3aaf
Matis Masters <matis@craftedcode.com>
2016-12-12 09:14:17 -0300

    Make operator public

--- app/controllers/operators_controller.rb
+++ app/controllers/operators_controller.rb
@@ -1,4 +1,4 @@
-class OperatorsController < SecuredApplicationController
+class OperatorsController < ApplicationController 
   def last_read_from_all_devices
     render json: { reads: ReadingEvent.last_read_from_all_devices }, status: 200
   end
