38318cf1996f6255777c845e151bcd3cb4e0b7cf
Matis Masters <me@matis.io>
2018-06-21 01:07:04 -0300

    Fix rubocop for controllers/application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,31 +15,30 @@ class ApplicationController < ActionController::Base
   private
 
   def cors_set_access_control_headers
-   headers['Access-Control-Allow-Origin'] = '*'
-   headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
-   headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token'
-   headers['Access-Control-Max-Age'] = '1728000'
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
+    headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token'
+    headers['Access-Control-Max-Age'] = '1728000'
   end
 
   def cors_preflight_check
-   if request.method == 'OPTIONS'
-     headers['Access-Control-Allow-Origin'] = '*'
-     headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
-     headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version, Token'
-     headers['Access-Control-Max-Age'] = '1728000'
+    return unless request.method == 'OPTIONS'
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
+    headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version, Token'
+    headers['Access-Control-Max-Age'] = '1728000'
 
-     render :text => '', :content_type => 'text/plain'
-   end
+    render text: '', content_type: 'text/plain'
   end
 
   def add_allow_credentials_headers
-   # https://developer.mozilla.org/en-US/docs/Web/HTTP/Access_control_CORS#section_5
-   #
-   # Because we want our front-end to send cookies to allow the API to be authenticated
-   # (using 'withCredentials' in the XMLHttpRequest), we need to add some headers so
-   # the browser will not reject the response
-   response.headers['Access-Control-Allow-Origin'] = request.headers['Origin'] || '*'
-   response.headers['Access-Control-Allow-Credentials'] = 'true'
+    # https://developer.mozilla.org/en-US/docs/Web/HTTP/Access_control_CORS#section_5
+    #
+    # Because we want our front-end to send cookies to allow the API to be authenticated
+    # (using 'withCredentials' in the XMLHttpRequest), we need to add some headers so
+    # the browser will not reject the response
+    response.headers['Access-Control-Allow-Origin'] = request.headers['Origin'] || '*'
+    response.headers['Access-Control-Allow-Credentials'] = 'true'
   end
 
   def parameter_missing_response(exception)
